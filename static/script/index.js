db4744b3edc4ff07387604007ba2b8380e1b3f9c
Phuntsho <Phuntsho@github.com>
2019-02-09 16:50:41 -0500

    the bird and background moving

--- static/script/index.js
+++ static/script/index.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-    $("#home").click(function(){
-        $("#sound").remove();
-        console.log("Donzo")
+    console.log("Testing if the script file is running");
+    $(".start").click(function(){
+        $(".start").hide("fast");
+        start();
+        console.log("button hidden");
     })
 });
+function start(){
+    console.log("started: All ready to go");
+    $("#in-game_title").hide("fast");
+    $(".canvas").css({
+        "animation": "bkg_move ease-in 5s"
+    });
+    $("#bird").css({
+        "top": "200px",
+        "animation": "bird_move ease-in 5s"
+    });
+    $(document).keydown(function(event) {
+        if ( event.which === 13 ) {
+          check();
+        }
+    });
+}
+
+function check(){
+    $("#bird").css("animation-play-state","paused");
+    $(".canvas").css("animation-play-state","paused");
+
+}
