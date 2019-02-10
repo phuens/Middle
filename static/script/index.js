adc0f2a43a864410073935982d355d0549482e19
Phuntsho <Phuntsho@github.com>
2019-02-10 02:01:50 -0500

    returns true if the bird is in the cage and false if it is outside

--- static/script/index.js
+++ static/script/index.js
@@ -14,20 +14,37 @@ function start(){
         "animation": "bkg_move ease-in 5s"
     });
     $("#bird").css({
-        "animation": "bird_move ease-in 5s"
+        "animation": "bird_move ease-in 10s"
     });
+    userInput();
+
+}
+
+function userInput(){
     $(document).keydown(function(event) {
         if ( event.which === 13 ) {
-          check();
+            check();
         }
     });
 }
 
 function check(){
-    $("#bird").css("animation-play-state","paused");
+    let bird = $("#bird");
+    bird.css("animation-play-state","paused");
     $(".canvas").css("animation-play-state","paused");
-    let position = $("#cage").offset();
-    console.log("Cage left: " + position.left + " top: " + position.top);
-    let bird_position = $("#bird").offset();
+
+    //position of the cage
+    let cage_position = $("#cage").offset();
+    console.log("Cage left: " + cage_position.left + " top: " + cage_position.top);
+
+    //position of the bird when stopped.
+    let bird_position = bird.offset();
     console.log("Bird left: " + bird_position.left + " top: " + bird_position.top)
+
+    if (bird_position.left > cage_position.left-20 && bird_position.left < cage_position.left+150 ){
+        console.log("right")
+    }
+    else{
+        console.log("Failed")
+    }
 }
